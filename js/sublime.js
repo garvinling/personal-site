37373c7c8b36a6eade3d742987450fb4848e4520
scalkpdev <garvin.ling@kp.org>
2016-02-18 17:15:11 -0800

    Removing on click style change
    Removing styling change on click for parent folder

--- js/sublime.js
+++ js/sublime.js
@@ -6,7 +6,18 @@ var projects = [
 		role : 'design + full-stack dev',
 		link : 'coming soon',
 		repo : 'https://github.com/garvinling/code-meets-coffee',
-		tech : 'MeteorJS , React'
+		tech : 'MeteorJS , React',
+		desc : 'A tinder-like web app using the Github API for discovering open source projects.'
+	},
+	{
+		type : 'work',
+		name : 'CoffeeDisplay',
+		role : 'architecture + full-stack dev',
+		link : 'private',
+		repo : 'private',
+		tech : 'Node(Express) , Angular',
+		desc : 'News-feed for digital signage that pulls in Twitter data and allows users to post content to be displayed.'
+
 	},
 	{
 		type : 'work',
@@ -14,17 +25,32 @@ var projects = [
 		role : 'architecture + full-stack dev',
 		link : 'coming soon',
 		repo : 'private',
-		tech : 'Node(Express) , Angular'
+		tech : 'Node(Express) , Angular',
+		desc : 'Project management web app for all projects in the Southern California region.'
+
 	},
+	{
+		type : 'work',
+		name : 'KP WalkOn',
+		role : 'led dev efforts for enhancements feature set',
+		link : 'coming soon',
+		repo : 'private',
+		tech : 'Node(Express) , Angular',
+		desc : 'Dashboard to track physical activity steps of patients.'
+
+	},
+
+
 
 
 ]
 
 
-var li = document.getElementById('project-dir').getElementsByTagName('li');
-var projectContent = document.getElementById('project-render');
-var source = document.getElementById('project-template');
-var template = Handlebars.compile(source.innerHTML);
+var li                   = document.getElementById('project-dir').getElementsByTagName('li');
+var workli               = document.getElementById('project-dir').getElementsByTagName('li');
+var projectContent       = document.getElementById('project-render');
+var source               = document.getElementById('project-template');
+var template             = Handlebars.compile(source.innerHTML);
 projectContent.innerHTML = template(projects[0]);
 
 
@@ -54,11 +80,14 @@ function addEventListeners() {
 
 
 			li[i].onclick = function(){
+				if(index > 0 ) {
 
-				removeAllClasses();
-				this.classList.add('project-selected');
-				projectContent.innerHTML = template(projects[index-1]);
-			  
+					removeAllClasses();
+					this.classList.add('project-selected');
+					projectContent.innerHTML = template(projects[index-1]);
+				  
+				}
+ 
 			  }    
 		})(i);
 
